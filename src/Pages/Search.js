0244c7b053725bad17f63459b96f9fa73999af33
atul patil <atulpatil539@gmail.com>
2021-11-25 10:42:16 +0530

    style: home search updated

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -2,16 +2,20 @@ import React from 'react'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
+import "./Search.css"
 function Search() {
     return (
         <form className="search">
         <div className="search__input">
-            <SearchIcon />
+            <SearchIcon 
+            className="searchicon" />
             <input />
             <MicIcon />
-            <Button type="submit" variant="outlined">Google Search</Button>
-            <Button variant="outlined">I'm Feeling Lucky</Button>
 
+        </div>
+        <div className="search__buttons">
+        <Button type="submit" variant="outlined">Google Search</Button>
+            <Button variant="outlined">I'm Feeling Lucky</Button>
         </div>
         </form>
     )
