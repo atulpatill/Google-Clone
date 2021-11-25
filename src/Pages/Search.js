1d8b252b4953fc323ca136e55235c2cfbd231a52
atul patil <atulpatil539@gmail.com>
2021-11-25 10:07:31 +0530

    feat: Search input added

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
 function Search() {
     return (
-        <div>
-        <p>Search</p>
+        <form className="search">
+        <div className="search__input">
+            <SearchIcon />
+            <input />
+            <MicIcon />
+            <Button type="submit" variant="outlined">Google Search</Button>
+            <Button variant="outlined">I'm Feeling Lucky</Button>
+
         </div>
+        </form>
     )
 }
 
