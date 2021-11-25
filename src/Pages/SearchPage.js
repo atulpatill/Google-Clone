8e6f0392950ca585e489fcf6b912728f3fa31c28
atul patil <atulpatil539@gmail.com>
2021-11-25 16:15:23 +0530

    style: search page styling

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -7,6 +7,7 @@ import ImageIcon from '@material-ui/icons/Image';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import RoomIcon from '@material-ui/icons/Room';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import "./SearchPage.css"
 
 
 function SearchPage() {
@@ -16,45 +17,47 @@ function SearchPage() {
               <Link to="/">
                   <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"/>
               </Link>
-          </div>
+         
           <div className="searchPage__headerBody">
               <Search hidebuttons />
-          </div>
+         
           <div className="searchPage__options">
           <div className="searchPage__optionsLeft">
-          <div className="searchPage__options">
+          <div className="searchPage__option">
              <SearchIcon />
              <Link to="/all">All</Link>
           </div>
-          <div className="searchPage__options">
+          <div className="searchPage__option">
             <DescriptionIcon />
             <Link to="/all">News</Link>
           </div>
-          <div className="searchPage__options">
+          <div className="searchPage__option">
             <ImageIcon/>
             <Link to="/all">Images</Link>
           </div>
-          <div className="searchPage__options">
+          <div className="searchPage__option">
             <LocalOfferIcon />
             <Link to="/all">Shopping</Link>
           </div>
-          <div className="searchPage__options">
+          <div className="searchPage__option">
             < RoomIcon/>
             <Link to="/all">Maps</Link>
           </div>
-          <div className="searchPage__options">
+          <div className="searchPage__option">
             < MoreVertIcon/>
             <Link to="/all">More</Link>
           </div>
           
         </div>
         <div className="searchPage__optionsRight">
-        <div className="searchPage__options">
+        <div className="searchPage__option">
         <Link to="/settings">Settings</Link>
           </div>
-          <div className="searchPage__options">
+          <div className="searchPage__option">
            <Link to="/tools">Tools</Link>
           </div>
+        </div>
+        </div>
         </div>
         </div>
         </div>
