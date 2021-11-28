be34b96a0c062c787702655f2df008c37dd6c673
atul patil <atulpatil539@gmail.com>
2021-11-28 23:03:04 +0530

    feat: state updated

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 import "./Search.css"
+import { useHistory } from 'react-router';
 function Search({hidebuttons}) {
+
+    const [input, setInput] = useState("") 
+
+    const history = useHistory()
+
+    const search=(e)=>{
+        e.preventDefault();
+        history.push("/search")
+    } 
+
+  
+
     return (
         <form className="search">
         <div className="search__input">
             <SearchIcon 
             className="searchicon" />
-            <input />
+            <input  value={input} onChange={(e)=> setInput(e.target.value)}/>
             <MicIcon />
 
         </div>
       {
             !hidebuttons &&   
             <div className="search__buttons">
-            <Button type="submit" variant="outlined">Google Search</Button>
+            <Button onClick={search} type="submit" variant="outlined">Google Search</Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
       }
